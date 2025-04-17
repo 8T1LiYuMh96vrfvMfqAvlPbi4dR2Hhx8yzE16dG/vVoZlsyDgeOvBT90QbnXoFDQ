64f62afbc7bff1e03cf39b15c42b9006a9c3fbcb
SamirOnly <klyo.inq@gmail.com>
2025-04-17 03:20:45 +0200

    Update BGSM.lua

--- b8z3Qp9lJ4x7k2mD5tnvC6hwYr/BGSM.lua
+++ b8z3Qp9lJ4x7k2mD5tnvC6hwYr/BGSM.lua
@@ -15,15 +15,22 @@ local Kaugummi = {
         ["Dashboard"] = {
             ["auto_bubblegum"] = false;
             ["auto_sell_bubblegum"] = false;
+            ["auto_sell_max"] = false;
             ["auto_buy_next_flavor"] = false;
             ["auto_buy_next_gum"] = false;
+            ["auto_sell_custom"] = nil;
+            ["auto_sell_custom_enabled"] = false;
         };
         ["Misc"] = {
             ["auto_claim_prizes"] = false;
             ["auto_claim_daily"] = false;
             ["unlock_all_islands"] = false;
             ["auto_claim_playtime"] = false;
+            ["auto_claim_wheel_spin"] = false;
             ["auto_coins_potion"] = false;
+            ["auto_hatch"] = false;
+            ["choosen_egg"] = nil;
+            ["auto_collect_coins"] = false;
         };
     };
     UI = {
@@ -50,13 +57,17 @@ function bubble:ClaimDailyReward()
     local all = {
         "DailyRewardClaimStars";
     };
+    getRemote:FireServer(table.find(all, "DailyRewardClaimStars"));
+end;
 
-    getRemote:FireServer(table.find(all, "DailyRewardClaimStars")); print(tostring(self));
+function bubble:ClaimWheelSpin()
+    local getRemoteK = network:WaitForChild("Remote"):WaitForChild("Function");
+    local k;
+    getRemoteK:InvokeServer("WheelSpin");
 end;
 
 function bubble:ClaimPlaytime()
     local getRemoteJ = network:WaitForChild("Remote"):WaitForChild("Function");
-
     for i = 1, 9 do 
         task.wait()
         getRemoteJ:InvokeServer("ClaimPlaytime", tonumber(i))
@@ -65,17 +76,26 @@ end;
 
 function bubble:UseAllCoinPotions()
     getRemoteK = network:WaitForChild("Remote"):WaitForChild("Event");
-
     local args = {
         [1] = "UsePotion",
         [2] = "Coins",
         [3] = 3
     }
-
     for i = 1, 5 do 
         task.wait()
         getRemoteK:FireServer("UsePotion", "Coins", tonumber(i))
     end;
+end;
+
+function bubble:GetAllEggs()
+    local getNga = replicatedstorage.Assets.Eggs;
+    local eggs = {};
+    for _, v in pairs(getNga:GetChildren()) do 
+        if v:IsA("Model") then
+            table.insert(eggs, v.Name);
+        end;
+    end;
+    return eggs;
 end;
 
 function bubble:GetIslands()
@@ -88,7 +108,6 @@ end;
 
 function bubble:ClaimPrize()
     getRemoteI = network:WaitForChild("Remote"):WaitForChild("Event");
-
     for i = 1, 20 do 
         task.wait()
         getRemoteI:FireServer("ClaimPrize", tonumber(i));
@@ -99,17 +118,25 @@ function bubble:BlowBubbles()
     local getRemote = network:WaitForChild("Remote"):WaitForChild("Event");
     local g;
     local arg = "BlowBubble"
-    
     while (Kaugummi.Config.Dashboard.auto_bubblegum and getRemote) do task.wait()
         getRemote:FireServer(tostring(arg))
     end;
+end;
+
+function bubble:CollectCoins()
+    for i, v in pairs(workspace.Rendered:GetChildren()[12]:GetChildren()) do
+        if v:IsA("Model") then
+            local id = v.Name
+            game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Pickups"):WaitForChild("CollectPickup"):FireServer(id)
+            task.wait()
+        end;
+    end;  
 end;
 
 function bubble:SellBubbles()
     local getRemoteH = network:WaitForChild("Remote"):WaitForChild("Event");
     local h;
     local arg1 = "SellBubble"
-
     while (Kaugummi.Config.Dashboard.auto_sell_bubblegum and getRemoteH) do task.wait()
         getRemoteH:FireServer(tostring(arg1))
     end;
@@ -123,20 +150,32 @@ function bubble:GetAllCodes()
     return codes;
 end;
 
-function bubble:GetAllEggs()
-    local getF = workspace.Rendered.Generic;
-    local i;
-
-    for k,v in pairs(getF:GetChildren()) do 
-        if v:IsA("Model") and v:FindFirstChild("Hitbox") then 
-            return v;
-        end;
+function bubble:RedeemAllCodes()
+    local codes = self:GetAllCodes()
+    local getRemote = network:WaitForChild("Remote"):WaitForChild("Function");
+    for _, code in ipairs(codes) do
+        local args = { "RedeemCode", code }
+        local success, result = pcall(function()
+            return getRemote:InvokeServer(unpack(args))
+        end)
+        if success then print("Redeemed Code:", code)
+        else warn("Failed to redeem code:", code, result) end
+        task.wait(.5);
     end;
 end;
 
+function bubble:AntiAfk()
+    local vu = game:GetService("VirtualUser");
+    
+    Client.Idled:Connect(function()
+        vu:Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+        task.wait(1)
+        vu:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+    end)
+end;
+    
 local function GetCoins()
-    local label = client:WaitForChild("PlayerGui"):WaitForChild("ScreenGui"):WaitForChild("HUD"):WaitForChild("Left"):WaitForChild("Currency"):WaitForChild("Coins"):WaitForChild("Frame"):WaitForChild("Label")
-
+    local label = client:WaitForChild("PlayerGui"):WaitForChild("ScreenGui"):WaitForChild("HUD"):WaitForChild("Left"):WaitForChild("Currency"):WaitForChild("Coins"):WaitForChild("Frame"):WaitForChild("Label");
     local rawText = label.Text:gsub(",", "");
     return tonumber(rawText) or 0;
 end;
@@ -157,60 +196,69 @@ local function GetSortedFlavors()
 end;
 
 local flavorData = require(replicatedstorage.Shared.Data.Flavors)
-
 function bubble:AutoBuyNextFlavor()
     local coins = GetCoins()
-    local remote = network:WaitForChild("Remote"):WaitForChild("Event")
+    local remote = network:WaitForChild("Remote"):WaitForChild("Event");
 
     local flavors = {}
     for name, data in pairs(flavorData) do
-        local costData = data.Cost
-        local cost = type(costData) == "table" and costData.Currency == "Coins" and costData.Amount or 0
-        table.insert(flavors, { Name = name, Cost = cost })
-    end
+        local costData = data.Cost;
+        local cost = type(costData) == "table" and costData.Currency == "Coins" and costData.Amount or 0;
+        table.insert(flavors, { Name = name, Cost = cost });
+    end;
 
     table.sort(flavors, function(a, b)
-        return a.Cost < b.Cost
+        return a.Cost < b.Cost;
     end)
 
     for _, flavor in ipairs(flavors) do
         if coins >= flavor.Cost then
             remote:FireServer("GumShopPurchase", flavor.Name)
-            task.wait(1)
+            task.wait(1);
         else
-            break
-        end
-    end
-end
+            break;
+        end;
+    end;
+end;
 
-
+function bubble:AutoLoad()
+    if identifyexecutor() then
+        pcall(function()
+            task.defer(function()
+                print("in queue..");
+                queue_on_teleport([[loadstring(game:HttpGet("https://raw.githubusercontent.com/TuahScripts/AIuisdgbs-ijbisUDbgisebjHLXJCgdsiutebsKSdge/refs/heads/main/Loader"))()]])
+            end);
+        end);
+    else
+        print("Something went wrong, AutoLoad Issue", type(queue_on_teleport))
+    end;
+end;
 
 local gumData = require(replicatedstorage.Shared.Data.Gum)
-
 function bubble:AutoBuyNextGum()
     local coins = GetCoins()
-    local remote = network:WaitForChild("Remote"):WaitForChild("Event")
+    local remote = network:WaitForChild("Remote"):WaitForChild("Event");
 
     local gums = {}
     for name, data in pairs(gumData) do
         local costData = data.Cost
         local cost = type(costData) == "table" and costData.Currency == "Coins" and costData.Amount or 0
         table.insert(gums, { Name = name, Cost = cost })
-    end
+    end;
 
     table.sort(gums, function(a, b)
-        return a.Cost < b.Cost
+        return a.Cost < b.Cost;
     end)
 
     for _, gum in ipairs(gums) do
         if coins >= gum.Cost then
             remote:FireServer("GumShopPurchase", gum.Name)
-            task.wait(1)
+            task.wait(1);
         else
-            break
-        end
-    end
-end
+            break;
+        end;
+    end;
+end;
 
 --// UI
 local Repo = "https://raw.githubusercontent.com/8T1LiYuMh96vrfvMfqAvlPbi4dR2Hhx8yzE16dG"
@@ -239,6 +287,7 @@ local Sections = {
     Themes = Tabs.Config:CreateSection({ Title = "Themes", Side = "Left",}),
     Teleport = Tabs.Misc:CreateSection({ Title = "Teleport", Side = "Right",}),
     Potions = Tabs.Misc:CreateSection({ Title = "Potion", Side = "Left",}),
+    Hatch = Tabs.Misc:CreateSection({ Title = "Hatch", Side = "Left",}),
 };
 
 local Dashboard__AutoBlow = Sections.Main:CreateToggle({
@@ -273,6 +322,42 @@ local Dashboard__AutoSell = Sections.Main:CreateToggle({
         end;
     end;
     Flag = "auto_sell";
+})
+
+local Dashboard__AutoSell = Sections.Main:CreateToggle({
+    Text = "Auto Sell on MAX";
+    Subtext = "Sells only if Max Bubbles";
+    Alignment = "Left";
+    Default = false;
+    Callback = function(v)
+        Kaugummi.Config.Dashboard.auto_sell_max = v;
+
+        if v then 
+            task.spawn(function()
+                while Kaugummi.Config.Dashboard.auto_sell_max do
+                    local bubbleLabel = client.PlayerGui.ScreenGui.HUD.Left.Currency.Bubble.Frame.Label
+                    if bubbleLabel then
+                        local text = bubbleLabel.ContentText or ""
+                        local current, max = text:match("([%d,]+)%s*/%s*([%d,]+)")
+                        if current and max then
+                            current = current:gsub(",", ""):gsub("%s", "")
+                            max = max:gsub(",", ""):gsub("%s", "")
+                            local currentNum = tonumber(current)
+                            local maxNum = tonumber(max)
+                            if currentNum and maxNum and currentNum >= maxNum then
+                                local args = {
+                                    [1] = "SellBubble"
+                                }
+                                game:GetService("ReplicatedStorage"):WaitForChild("Shared"):WaitForChild("Framework"):WaitForChild("Network"):WaitForChild("Remote"):WaitForChild("Event"):FireServer(unpack(args))
+                            end;
+                        end;
+                    end;
+                    task.wait(1);
+                end
+            end)
+        end;
+    end;
+    Flag = "auto_sell_max";
 })
 
 local Dashboard__AutoBuyNextFlavor = Sections.Shop:CreateToggle({
@@ -365,6 +450,42 @@ local Misc__AutoClaimPlaytime = Sections.Misc:CreateToggle({
     Flag = "auto_claim_playtime";
 })
 
+local Misc__AutoClaimWheel = Sections.Misc:CreateToggle({
+    Text = "Auto Spin Wheel";
+    Subtext = "";
+    Alignment = "Left";
+    Default = false;
+    Callback = function(v)
+        Kaugummi.Config.Misc.auto_claim_wheel_spin = v;
+
+        while Kaugummi.Config.Misc.auto_claim_wheel_spin do
+            task.wait(1);
+            task.spawn(function()
+                bubble:ClaimWheelSpin()       
+            end)
+        end;
+    end;
+    Flag = "auto_claim_wheel_spin";
+})
+
+local Misc__AutoCollectCoins = Sections.Misc:CreateToggle({
+    Text = "Auto Collect Coins";
+    Subtext = "";
+    Alignment = "Left";
+    Default = false;
+    Callback = function(v)
+        Kaugummi.Config.Misc.auto_collect_coins = v;
+
+        while Kaugummi.Config.Misc.auto_collect_coins do
+            task.wait(1);
+            task.spawn(function()
+                bubble:CollectCoins()       
+            end)
+        end;
+    end;
+    Flag = "auto_collect_coins";
+})
+
 local Misc__UseAllCoinsPotion = Sections.Potions:CreateToggle({
     Text = "Auto use Coins Potion";
     Subtext = "";
@@ -382,6 +503,17 @@ local Misc__UseAllCoinsPotion = Sections.Potions:CreateToggle({
     end;
     Flag = "auto_coins_potion";
 })
+
+local Misc__RedeemAllCodes = Sections.Misc:CreateButton({
+    Text = "Redeem All Codes";
+    Alignment = "Left";
+    Callback = function()
+        task.spawn(function()
+            bubble:RedeemAllCodes()
+        end)
+    end;
+})
+
 
 local Misc__UnlockAllIslands; Misc__UnlockAllIslands = Sections.Teleport:CreateButton({
     Text = "Unlock all Islands";
@@ -469,7 +601,7 @@ local AutoLoad_Toggle = Sections.Data:CreateToggle({
     Callback = function(al)
         if al then 
             task.spawn(function()
-                hunters.AutoLoad()
+                bubble:AutoLoad();
             end);
         end;
     end;
@@ -539,6 +671,72 @@ local Config__DarkTheme = Sections.Themes:CreateToggle({
 
 local Config__AvailableThemes = Sections.Themes:CreateLog({
     Default = { "Dark", "Light", "Midnight", "Bloom", "Minecraft", "Windows 11", "Lotus Dark", "Default" };
+})
+
+
+local Misc__HatchableEggs = Sections.Hatch:CreateDropdown({
+    Text = "Choose Egg";
+    Subtext = "";
+    Alignment = "Left";
+    Choices = bubble:GetAllEggs();  
+    Multi = false;
+    Default = nil;
+    Callback = function(v) 
+        Kaugummi.Config.Misc.choosen_egg = v
+    end;
+    Flag = "choosen_egg";
+})
+
+local Misc__HatchEgg; Misc__HatchEgg = Sections.Hatch:CreateToggle({
+    Text = "Auto Open";
+    Subtext = "Hatches the egg for you";
+    Alignment = "Left";
+    Default = false;
+    Callback = function(enabled) 
+        Kaugummi.Config.Misc.auto_hatch = enabled
+
+        task.spawn(function()
+            while Kaugummi.Config.Misc.auto_hatch do
+                task.wait(1)
+                if Kaugummi.Config.Misc.choosen_egg then
+                    local args = {
+                        [1] = "HatchEgg",
+                        [2] = tostring(Kaugummi.Config.Misc.choosen_egg),
+                        [3] = 1
+                    }
+                network:WaitForChild("Remote"):WaitForChild("Event"):FireServer(unpack(args))
+                else
+                    warn("No egg selected")
+                end
+            end
+        end)
+    end;
+    Flag = "auto_hatch";
+})
+
+local Misc__Refresh = Sections.Hatch:CreateButton({
+    Text = "Refresh Eggs";
+    Callback = function()
+        local reEggs = bubble:GetAllEggs();
+
+        task.wait(1)
+        Misc__HatchEgg:SetOptions(reEggs);
+    end;
+})
+
+local Config__AntiAfk = Sections.Data:CreateToggle({
+    Text = "Anti Afk";
+    Subtext = "";
+    Alignment = "Left";
+    Default = false;
+    Callback = function(a)
+        if a then 
+            task.spawn(function()
+                bubble:AntiAfk();
+            end)
+        end;
+    end;
+    Flag = "anti_afk";
 })
 
 Library:Notify("Script loaded for: \n" ..tostring(bubble:GetGame()), 5, "Tuah")
